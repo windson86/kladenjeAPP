f911142e6dbc9ba036bc98f77fdb67e08aaf914f
ivekw <petrovic1986@gmail.com>
2021-07-07 13:01:42 +0200

    promjenaservera

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ require("./rute/api")(app);
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'));
-
+    app.get('*', (req,res) =>{
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
 }
 
 app.listen(PORT, console.log(`Server je pokrenut na portu: ${PORT}.`));
